4bbebfa642ad0f2f145bc51015634ac7a094a3a0
Joice Cazanoski Gomes <cgjoice@gmail.com>
2018-06-25 11:18:41 -0300

    Update newCode.cs

--- newCode.cs
+++ newCode.cs
@@ -1,19 +1,17 @@
-﻿class newCode
+class newCode
 {
     public static string removeEnter(string strText)
     {
         string[] verify = { "\n", "\r", "\r\n" };
-
         foreach (string enter in verify)
         {
             if (strText.Contains(enter))
             {
                 strText = strText.Replace(enter, " ");
             }
-        }
-      
+        }      
+        
         return strText;
-
     }
 }
 
