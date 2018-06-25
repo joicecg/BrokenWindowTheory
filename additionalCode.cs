29a4b6566f783a322542be326c084bc3731c1a27
Joice Cazanoski Gomes <joice.gomes@ska.com.br>
2018-06-25 11:15:13 -0300

    remove unuded code

--- additionalCode.cs
+++ additionalCode.cs
@@ -1,10 +1,4 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-
-class additionalCode
+﻿class additionalCode
 {
     public string removeEnterAndSpaces(string strText)
     {
--- badCode.cs
+++ badCode.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-class badCode
+﻿class badCode
 {
     public static string removeENTERdetexto(string strvalor)
     {
